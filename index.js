313f772008d60ef08a41d295c8131c669fa299f0
emily <mle4d@me.com>
2019-06-26 13:56:16 -0700

    json changes

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const { createServer } = require('http');
 
 const server = createServer((req, res) => {
-console.log('client made request')
-console.log('req');
-res.end('hi there');
+  console.log('client made request')
+  console.log('req');
+  res.statusCode = 200;
+  res.setHeader('Content-type', 'text-plain');
+  res.end('hola');
 });
 
-server.listen(666, () => {
+server.listen(8080, () => {
   console.log('listening');
-})
+});
