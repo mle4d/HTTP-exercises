adfb9c7c58e1efc8a0b9d4d183e08fe3df1fa163
emily <mle4d@me.com>
2019-06-26 14:05:28 -0700

    added html

--- index.js
+++ index.js
@@ -5,7 +5,23 @@ const server = createServer((req, res) => {
   console.log('req');
   res.statusCode = 200;
   res.setHeader('Content-type', 'text-plain');
-  res.end('hola');
+  res.end(`<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hello</title>
+  </head>
+  <body>
+  <ul>
+  <li>Hey</li>
+  <li>Hi</li>
+  <li>Hola</li>
+  </ul>
+    
+  </body>
+  </html>`);
 });
 
 server.listen(8080, () => {
